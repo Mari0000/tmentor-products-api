ddf8dc8e88818ee3b8e09ba045e80fe2b932e317
mariosobhy <mariosobhy0123@outlook.com>
2020-08-28 01:04:16 +0200

    Add matching pattern for name

--- controllers/product.js
+++ controllers/product.js
@@ -4,13 +4,21 @@ const Product = require("../models/product");
 module.exports = {
     search: async (req, res) => {
       try {
-          let query = req.body;
+          let query = req.query;
+          if(query.name){
+            query["name"] = { "$regex": query.name, "$options": "i" }
+          }
           // handling price search 
           if(query.lowerPrice && query.upperPrice){
             query["price"] = {$gte:query.lowerPrice , $lte:query.upperPrice}
             delete query["upperPrice"];
             delete query["lowerPrice"];
-            }    
+          }
+          // handle category
+          if(query.category){
+            query["category.id"] = query.category
+            delete query["category"]
+          } 
         const products = await Product.find(query);
         res.send(products)
       } catch (error) {
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 require("../db/mongoose");
 const productRouter = require("../routers/product");
+var cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
 //const User = require("./models/user");
 
 
 app.use(express.json());
+app.use(cors());
 app.use(productRouter);
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
